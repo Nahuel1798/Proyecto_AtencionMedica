47f154bd9ea6502292a078aa55c25e0cbd7a4838
Nahuel1798 <142271485+Nahuel1798@users.noreply.github.com>
2024-12-12 18:30:37 -0300

    ultimaultima

--- controllers/navController.js
+++ controllers/navController.js
@@ -8,7 +8,16 @@ exports.listarMedicos = async (req, res) => {
 };
 
 exports.listarPacientes = async (req, res) => {
-
     const pacientes = await navModels.getObtenerPaciente();
-    res.render('mostrarPacientes', { pacientes, medicoId : req.session.medicoId }); 
+    const pacienteFormateada = pacientes.map(item => {
+        const fechapaciente = new Date(item.fecha_nacimiento);  // Cambiar "fecha_consulta" según el nombre del campo en tu base de datos
+        const dia = String(fechapaciente.getDate()).padStart(2, '0');
+        const mes = String(fechapaciente.getMonth() + 1).padStart(2, '0');
+        const anio = fechapaciente.getFullYear();
+        const fechaFormateada = `${dia}/${mes}/${anio}`;
+    
+        // Retornar el objeto original con la fecha formateada
+        return { ...item, fecha_nacimiento: fechaFormateada };
+      });
+    res.render('mostrarPacientes', { pacientes: pacienteFormateada, medicoId : req.session.medicoId }); 
 };
